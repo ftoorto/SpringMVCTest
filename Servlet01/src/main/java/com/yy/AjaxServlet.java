c0cc219a9841c7a1d2e295d592260b9f8d398640
ftoorto <m.f.d.y.n.a@live.com>
2022-03-14 20:03:13 +0800

    ajax servlet交互2

---
+++ Servlet01/src/main/java/com/yy/AjaxServlet.java
@@ -0,0 +1,28 @@
+package com.yy;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "AjaxServlet", value = "/AjaxServlet")
+public class AjaxServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("utf-8");
+        response.setHeader("Access-Control-Allow-Origin","*");
+        response.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE");
+        response.setContentType("text/html;charset=utf-8");
+
+        String No= (String) request.getAttribute("no");
+        String Name=(String) request.getAttribute("name");
+        System.out.println("No: "+No+"\n"+"Name: " +Name);
+        response.getWriter().print("hahaha");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
