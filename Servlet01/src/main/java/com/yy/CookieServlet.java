011489ebe36cfc34f5f31483d8ab99b1fb68cdd2
ftoorto <m.f.d.y.n.a@live.com>
2022-03-22 22:47:55 +0800

    cookie 一次只能加一个，为什么？

--- Servlet01/src/main/java/com/yy/CookieServlet.java
+++ Servlet01/src/main/java/com/yy/CookieServlet.java
@@ -12,13 +12,24 @@ import java.util.Date;
 public class CookieServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+
         SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss a");
         String date=simpleDateFormat.format(new Date());
         Cookie cookie1=new Cookie("name","aaa");
-        Cookie cookie2=new Cookie("日期",date);
+        Cookie cookie2=new Cookie("rst",date);
         response.addCookie(cookie1);
-        response.addCookie(cookie2);
+        response.addCookie(cookie2);// TODO 为何不能一次加两个cookie
         Cookie[] cookies=request.getCookies();
+        if (cookies!=null){
+            for (Cookie cookie:cookies){
+                System.out.println("Cookie: "+cookie+"\n");
+            }
+        }
+        else {
+            System.out.println("第一次访问，未携带cookie");
+        }
 
     }
 
