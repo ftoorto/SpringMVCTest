44e323822702b543bd775f1016546acd75408d63
ftoorto <m.f.d.y.n.a@live.com>
2022-03-30 00:36:55 +0800

    Session update

--- Servlet01/src/main/java/com/yy/SessionServlet.java
+++ Servlet01/src/main/java/com/yy/SessionServlet.java
@@ -5,13 +5,22 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 @WebServlet(name = "SessionServlet",value = "/SessionServlet")
 public class SessionServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getSession()
+
+
+        HttpSession session=req.getSession();
+        if(session.getAttribute("name")!=null){
+            System.out.println((String) session.getAttribute("name"));
+        }
+        else
+            System.out.println("name属性未创建");
+        session.setAttribute("name","Unevensea856");
 
     }
 
