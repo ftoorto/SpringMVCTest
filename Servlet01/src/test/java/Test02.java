594ee8020f85907ead29bcdc072f3f63dff19a34
ftoorto <m.f.d.y.n.a@live.com>
2022-05-12 00:35:47 +0800

    ConstantDesc

--- Servlet01/src/test/java/Test02.java
+++ Servlet01/src/test/java/Test02.java
@@ -1,15 +1,58 @@
 import com.yy.enums.Grade;
 import org.junit.Test;
 
+import java.beans.MethodDescriptor;
+import java.lang.constant.ClassDesc;
 import java.lang.constant.Constable;
+import java.lang.constant.ConstantDesc;
+import java.lang.constant.ConstantDescs;
+import java.util.Optional;
 
 public class Test02 {
+    public final String s="final haha";
     @Test
     public void method01(){
         Grade g=Grade.One;
         System.out.println("g的年级："+ g.grade);
         int length = Grade.values().length;
-        Constable String a="haha";
+        String a="haha";
+        Optional<String> s = a.describeConstable();
+        System.out.println(s.get());
+//        Optional<Grade> grade=Optional.empty();
+//        System.out.println(grade.get());
+//        Grade grade2=null;
+//        System.out.println(grade2.toString());
+        System.out.println(g.getDeclaringClass());
+        ClassDesc classDesc= ConstantDescs.CD_Enum.arrayType();
+    }
+
+    ConstantDesc constantDesc= s.
+    static void describe(Class<?> clazz, String pad, String leadin) {
+        if (clazz == null) return;
+        String type =
+                clazz.isInterface() ? "interface" :
+                        clazz.isArray() ? "array" :
+                                clazz.isPrimitive() ? "primitive" :
+                                        clazz.isEnum() ? "enum" :
+                                                "class";
+        System.out.printf("%s%s%s %s ( %s )%n",
+                pad, leadin, type, clazz.getSimpleName(), clazz.getName());
+        for (Class<?> interfaze : clazz.getInterfaces()) {
+            describe(interfaze, pad + "   ", "implements ");
+        }
+        describe(clazz.getComponentType(), pad + "   ", "elements are ");
+        describe(clazz.getSuperclass(), pad + "   ", "extends ");
+    }
+    static void describe(Class<?> clazz) {
+        describe(clazz, "", "");
+        System.out.println();
+    }
+    @Test
+    public void test3() {
+        describe(boolean[][].class);
+        describe(java.math.RoundingMode.class);
+        describe(java.util.ArrayList.class);
+        describe(void.class);
     }
 
 }
